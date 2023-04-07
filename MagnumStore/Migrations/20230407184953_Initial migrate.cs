34d5c620f3784d52ce86f8c5110aa41c252cd697
theAzizbek12 <wiut00012121@gmail.com>
2023-04-08 00:00:23 +0500

    database commit

---
+++ MagnumStore/Migrations/20230407184953_Initial migrate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MagnumStore.Migrations
+{
+    public partial class Initialmigrate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
